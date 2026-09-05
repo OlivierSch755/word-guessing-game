340639b22807cb8645600b80e6b8d4c9519a4bc6
Olivier Gni <52855190+OlivierSch755@users.noreply.github.com>
2026-09-05 18:03:23 +0200

    fix import paths

--- js/player2.js
+++ js/player2.js
@@ -1,5 +1,5 @@
 import {Game, WordTile,TileSlot } from "./system.js";
-import {DictLoader} from ".dictloader.js";
+import {DictLoader} from "./dictloader.js";
 import {Random, rangeFromZeroTo, StyleSetter} from "./utils.js";
 import {LocalMemory} from "./storage.js";
 
